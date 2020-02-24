cf491d70a4ce49301efdb1799ab45c26cce964ab
gabriel gianotti <gabrielgianotti@orgsystem.com.br>
2020-02-23 23:15:46 -0300

    structure create

---
+++ src/pages/details/Details.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+// import { Container } from './styles';
+
+export default class details extends Component {
+  render() {
+    return (
+      <>
+      <h4>details</h4>
+      </>
+    );
+  }
+}
---
+++ src/routes/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { BrowserRouter, Route, Switch, } from 'react-router-dom';
+
+import Home from "../pages/home/Home.js"
+import Details from "../pages/details/Details.js"
+
+export default function Routes() {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/"  exact component={Home} />
+        <Route path="/details" component={Details} />
+      </Switch>
+    </BrowserRouter>
+  );
+};
+
