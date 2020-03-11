0ab44b6906794f2ecc1c6e27510df5dafbb5f278
Gabriel Simon Gianotti <gabrielgianotti@outlook.com>
2020-03-11 08:10:30 -0300

    corrections

--- src/__test__/Home.test.js
+++ src/__test__/Home.test.js
@@ -1,24 +1,37 @@
 import React from "react";
 import { render, waitForElement, fireEvent } from "@testing-library/react"
 import Input from '../components/input/Input'
-import Index from '../App.js'
+
 describe('test for imput component', () => {
   it("input text", async () => {
-    const { getByTestId, getByText } = render(<Index />)
-
+    const { getByTestId, getByText } = render(
+      <Input
+        label="Pesquisa"
+        type="test"
+        list="browsers"
+        option={["a1", "a2"]}
+      />
+    )
     const imputNode = await waitForElement(
       () => getByTestId("form-input")
     )
     const newValue = "acer"
-
     fireEvent.change(
       imputNode,
       { target: { value: newValue } }
     )
     expect(imputNode.value).toEqual(newValue)
   })
+
   it("input text failed", async () => {
-    const { getByTestId, getByText } = render(<Index />)
+    const { getByTestId, getByText } = render(
+      <Input
+        label="Pesquisa"
+        type="number"
+        list="browsers"
+        option={["a1", "a2"]}
+      />
+    )
     let imputNode = await waitForElement(
       () => getByTestId("form-input")
     )
@@ -37,17 +50,21 @@ describe('test for imput component', () => {
   })
 
   it("generating options", async () => {
-    const { getByTestId, getByText } = render(<Index />)
+    const { getByTestId, getByText } = render(
+      <Input
+        label="Pesquisa"
+        type="test"
+        list="browsers"
+        option={["a1", "a2"]}
+      />
+    )
 
     const optionsNode = await waitForElement(() => getByTestId("form-option"))
-    let optionsValidate = true
-
+    let optionsValidate = false
 
     if (optionsNode.innerHTML.indexOf("a2") !== -1 && optionsNode.innerHTML.indexOf("a1") !== -1) {
       optionsValidate = true
     }
     expect(optionsValidate).toEqual(true)
-
   })
-
 })
